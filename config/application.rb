26a5918096d859e797450a9bda5661b32967135a
Jimmy Cuadra <jimmy@jimmycuadra.com>
2015-02-04 19:16:17 -0800

    Add layout, favicon, and GA.

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ Bundler.require(*Rails.groups)
 module Plugins
   class Application < Rails::Application
     config.autoload_paths += ['lib']
+    config.google_analytics = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
